11dadbb3ddc8180704e545327101f460699f7da5
bogdied <61083677+bogdied@users.noreply.github.com>
2022-11-20 23:22:44 +0200

    Update set_diff_intersect_union_symmdiff.cpp

--- L5_V1/set_diff_intersect_union_symmdiff.cpp
+++ L5_V1/set_diff_intersect_union_symmdiff.cpp
@@ -25,34 +25,41 @@ int main() {
     set_difference(first, second, f_s_difference);
     print_size_and_elements(f_s_difference);
 
-    std::vector<int> f_s_difference_stl;
-    std::set_difference(first.begin(), first.end(), second.begin(), second.end(), std::back_inserter(f_s_difference_stl));
-    print_size_and_elements(f_s_difference);
+//    std::vector<int> f_s_difference_stl;
+//    std::set_difference(first.begin(), first.end(), second.begin(), second.end(), std::back_inserter(f_s_difference_stl));
+//    print_size_and_elements(f_s_difference);
 
     std::vector<int> s_f_difference;
     set_difference(second, first, s_f_difference);
     print_size_and_elements(s_f_difference);
 
-    std::vector<int> s_f_difference_stl;
-    std::set_difference(second.begin(), second.end(), first.begin(), first.end(), std::back_inserter(s_f_difference_stl));
-    print_size_and_elements(s_f_difference_stl);
-
+//    std::vector<int> s_f_difference_stl;
+//    std::set_difference(second.begin(), second.end(), first.begin(), first.end(), std::back_inserter(s_f_difference_stl));
+//    print_size_and_elements(s_f_difference_stl);
 
     std::vector<int> intersection;
     set_intersection(first, second, intersection);
     print_size_and_elements(intersection);
 
+//    std::vector<int> intersection_stl;
+//    std::set_intersection(first.begin(), first.end(), second.begin(), second.end(), std::back_inserter(intersection_stl));
+//    print_size_and_elements(intersection_stl);
+
     std::vector<int> Union;
     set_union(first, second, Union);
     print_size_and_elements(Union);
 
-    std::vector<int> stl_union;
-    std::set_union(first.begin(), first.end(), second.begin(), second.end(), std::back_inserter(stl_union));
-    print_size_and_elements(Union);
+//    std::vector<int> stl_union;
+//    std::set_union(first.begin(), first.end(), second.begin(), second.end(), std::back_inserter(stl_union));
+//    print_size_and_elements(Union);
 
     std::vector<int> symmetrical_difference;
     set_symmetrical_difference(first, second, symmetrical_difference);
     print_size_and_elements(symmetrical_difference);
+
+//    std::vector<int> symmetrical_difference_stl;
+//    std::set_symmetric_difference(first.begin(), first.end(), second.begin(), second.end(), std::back_inserter(symmetrical_difference_stl));
+//    print_size_and_elements(symmetrical_difference_stl);
 
 
 }
@@ -84,6 +91,7 @@ void set_union(std::vector<int>& first, std::vector<int>& second, std::vector<in
 }
 
 void set_intersection(std::vector<int>& first, std::vector<int>& second, std::vector<int>& output){
+
     int i = 0, j = 0;
     while (i < first.size() && j < second.size()){
         if (first[i] > second[j])
@@ -97,7 +105,6 @@ void set_intersection(std::vector<int>& first, std::vector<int>& second, std::ve
         }
     }
 }
-
 void set_difference(std::vector<int>& first, std::vector<int>& second, std::vector<int>& output){
     int i = 0, j = 0;
     output = first;
